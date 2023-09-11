eaf6ea154b1054c81c06917966277776f3bccc03
Igor Siamykin <sparkisisss@gmail.com>
2023-09-11 18:35:33 +0300

    lesson 48

--- js/script.js
+++ js/script.js
@@ -1,152 +1,84 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };    
+    //////////////////////Tasks////////////////////////  
+    const advertisement = document.querySelectorAll('.promo__adv img'),
+          background = document.querySelector('.promo__bg'),
+          genre = background.querySelector('.promo__genre'),
+          filmsDB = document.querySelector('.promo__interactive-list'),
+          add = document.querySelector('form.add'),
+          input  = add.querySelector('.adding__input'),
+          checkbox = add.querySelector('[type="checkbox"]');         
+    //////////////////////////////////////////////
+    const delAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };   
 
-// Tasks 
-const advertisement = document.querySelectorAll('.promo__adv img'),
-      background = document.querySelector('.promo__bg'),
-      genre = background.querySelector('.promo__genre'),
-      filmsDB = document.querySelector('.promo__interactive-list');
+    const makeChanges = () => {
+        genre.textContent = 'драма';    
+        background.style.backgroundImage = 'url("img/bg.jpg")';
+    };    
 
-advertisement.forEach(item => {
-    item.remove();
-})
+    const sortArr = (arr) => {
+        arr.sort();
+    };    
+    
+    function createMovieList(films, parent) {
+        parent.innerHTML = '';  
+        sortArr(films);
 
-genre.textContent = 'драма';
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
 
-background.style.backgroundImage = 'url("img/bg.jpg")';
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
 
-filmsDB.innerHTML = '';
+                createMovieList(films, parent);
+            })
+        })
+    }    
+    //////////////////////////////////////////////
+    add.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const favorite = checkbox.checked;
+        let newFilm = input.value;
 
-movieDB.movies.forEach((film, i) => {
-    filmsDB.innerHTML += `
-        <li class="promo__interactive-item">${i} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
+        if(newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            if (favorite) {
+                console.log("A fab film!");
+            }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+    
+            createMovieList(movieDB.movies, filmsDB);
+        };     
+        event.target.reset();
+    });      
+    
+    makeChanges();
+    delAdv(advertisement);    
+    createMovieList(movieDB.movies, filmsDB);    
+       
 });
-//////////////////////////////////////////////
-genre.style.cursor = 'pointer';
-
-const delElement = (e) => {    
-    e.target.remove();
-};
-
-genre.addEventListener('click', delElement);
-////////////////////////////////////////////
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(filmsDB.firstElementChild);
-// for (let node of filmsDB.childNodes) {
-//     if (node.nodeName === '#text') {
-//         continue;
-//     }
-//     console.log(node);
-// }
-
-// let students = {
-//     js: [{
-//         name: 'John',
-//         progress: 100
-//     }, {
-//         name: 'Ivan',
-//         progress: 60
-//     }],
-
-//     html: {
-//         basic: [{
-//             name: 'Peter',
-//             progress: 20
-//         },{
-//             name: 'Ann',
-//             progress: 18
-//         }],
-
-//         pro: [{
-//             name: 'Sam',
-//             progress: 10
-//         }]
-//     }
-// };
-
-// function getTotalProgressByIteration(data) {
-//     let total = 0;
-//     let students = 0;
-
-//     for(let course of Object.values(data)) {
-//         if (Array.isArray(course)) {
-//             students += course.length;
-
-//             for (let i = 0; i < course.length; i++) {
-//                 total += course[i].progress;
-//             }
-//         }else {
-//             for(let subCourse of Object.values(course)) {
-//                 students += subCourse.length;
-
-//                 for (let i = 0; i < subCourse.length; i++) {
-//                     total += subCourse[i].progress;
-//                 }
-//             }
-//         }
-//     }
-
-//     return total / students;
-// }
-
-// console.log(getTotalProgressByIteration(students));
-
-function pow(x, n) {
-    if (n === 1) {
-        return x;
-    }else {
-        return x * pow(x, n - 1);
-    }
-}
-
-// const pr = pow(2, 6);
-// console.log(pr);
-
-
-function factorial(n) {
-    if (n % 1 !== 0) {
-        return "Число дробное";
-    }else if (n <= 0) {
-        return 1;
-    }else{
-        return n * factorial(n - 1);
-    }
-}
-
-let result = factorial(2.3);
-console.log(result);
-let result2 = factorial(0);
-console.log(result2);
-let result3 = factorial(4);
-console.log(result3);
-let result4 = factorial(-1);
-console.log(result4);
-let result5 = factorial(123.3);
-console.log(result5);
